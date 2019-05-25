550ef5a4f1b1dccb243798f1701fd98eaa406184
koekeishiya <aasvi93@hotmail.com>
2019-05-25 17:42:29 +0200

    ..

---
+++ src/sa.h
@@ -0,0 +1,9 @@
+#ifndef SA_H
+#define SA_H
+
+int scripting_addition_load(void);
+bool scripting_addition_is_installed(void);
+int scripting_addition_uninstall(void);
+int scripting_addition_install(void);
+
+#endif
