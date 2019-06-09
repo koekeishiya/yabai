17c87d4916c07da3f3e640cbe9f3d376265d6176
koekeishiya <aasvi93@hotmail.com>
2019-06-09 20:53:22 +0200

    only transmit signals for event that process successfully

--- src/event_loop.c
+++ src/event_loop.c
@@ -58,7 +58,7 @@ event_loop_run(void *context)
         struct event *event = queue_pop(queue);
         if (event) {
             int result = event_handler[event->type](event->context, event->param1, event->param2);
-            event_signal(event->type);
+            if (result == EVENT_SUCCESS) event_signal(event->type);
 
             if (event->status) *event->status = EVENT_PROCESSED;
             if (event->result) *event->result = result;
