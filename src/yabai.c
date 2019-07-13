edbca073edb4fa1cec8be8503e8efe816a7bfe7a
koekeishiya <aasvi93@hotmail.com>
2019-07-14 01:56:43 +0200

    cleanup

--- src/yabai.c
+++ src/yabai.c
@@ -294,9 +294,6 @@ int main(int argc, char **argv)
         notify("payload is not installed, some features will not work!", "scripting-addition");
     }
 
-    window_manager_check_for_windows_on_space(&g_space_manager, &g_window_manager, g_space_manager.current_space_id);
-    space_manager_refresh_view(&g_space_manager, g_space_manager.current_space_id);
-
     exec_config_file();
     CFRunLoopRun();
     return 0;
