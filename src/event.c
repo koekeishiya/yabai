a02ca96b2cc51aefdc4be57da825d54d0fed8105
koekeishiya <aasvi93@hotmail.com>
2019-07-31 00:23:50 +0200

    #118 invalidate space/view size when exiting mission-control

--- src/event.c
+++ src/event.c
@@ -1173,6 +1173,7 @@ static EVENT_CALLBACK(EVENT_HANDLER_MISSION_CONTROL_EXIT)
         }
     }
 
+    space_manager_mark_spaces_invalid(&g_space_manager);
     window_manager_validate_windows_on_space(&g_space_manager, &g_window_manager, g_space_manager.current_space_id);
     window_manager_check_for_windows_on_space(&g_space_manager, &g_window_manager, g_space_manager.current_space_id);
 
