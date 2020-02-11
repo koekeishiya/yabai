d67b05d64f5e187f5660446a2d6f493880d19e8c
koekeishiya <aasvi93@hotmail.com>
2020-02-11 08:29:27 +0100

    cleanup

--- src/process_manager.h
+++ src/process_manager.h
@@ -7,6 +7,21 @@ extern void _LSASNExtractHighAndLowParts(const void *asn, uint32_t *high, uint32
 extern CFTypeID _LSASNGetTypeID(void);
 #endif
 
+#define PROCESS_EVENT_HANDLER(name) OSStatus name(EventHandlerCallRef ref, EventRef event, void *user_data)
+typedef PROCESS_EVENT_HANDLER(process_event_handler);
+
+struct process
+{
+    ProcessSerialNumber psn;
+    pid_t pid;
+    char *name;
+    bool background;
+    bool lsuielement;
+    bool lsbackground;
+    bool xpc;
+    bool volatile terminated;
+};
+
 struct process_manager
 {
     struct table process;
@@ -19,6 +34,8 @@ struct process_manager
     ProcessSerialNumber finder_psn;
 };
 
+void process_destroy(struct process *process);
+struct process *process_create(ProcessSerialNumber psn);
 struct process *process_manager_find_process(struct process_manager *pm, ProcessSerialNumber *psn);
 void process_manager_remove_process(struct process_manager *pm, ProcessSerialNumber *psn);
 void process_manager_add_process(struct process_manager *pm, struct process *process);
