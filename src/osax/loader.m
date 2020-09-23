8d5b71ac6d955f5d8aac910bce87120ef96e5c49
koekeishiya <aasvi93@hotmail.com>
2020-09-23 10:44:12 +0200

    #589 get rid of osax Loader class definition

--- src/osax/loader.m
+++ src/osax/loader.m
@@ -2,20 +2,13 @@
 #import <Foundation/Foundation.h>
 #import "common.h"
 
-@interface Loader : NSObject
-@end
-
-@implementation Loader
-@end
-
 static Class _instance;
-
 OSErr yabai_osax_load(const AppleEvent *event, AppleEvent *reply, long context)
 {
     NSLog(@"[yabai] attempting to load scripting addition..");
 
     OSErr result = OSAX_PAYLOAD_SUCCESS;
-    NSBundle* loader_bundle = [NSBundle bundleForClass:[Loader class]];
+    NSBundle *loader_bundle = [NSBundle bundleWithIdentifier:@"com.koekeishiya.yabai-osax"];
     NSString *payload_path = [loader_bundle pathForResource:@"payload" ofType:@"bundle"];
     NSBundle *payload_bundle = [NSBundle bundleWithPath:payload_path];
 
