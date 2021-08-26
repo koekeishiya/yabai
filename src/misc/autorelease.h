4ab505dbe0d9aaf97476070ce40f8c73857f688f
koekeishiya <aasvi93@hotmail.com>
2021-08-26 13:54:58 +0200

    hook autorelease drain; print stack trace

--- src/misc/autorelease.h
+++ src/misc/autorelease.h
@@ -1,16 +1,49 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
+#include <execinfo.h>
 
 IMP g_nsobject_autorelease;
+IMP g_nsautoreleasepool_drain;
 
 @implementation NSObject(swizzle)
 - (NSObject *)fake_autorelease
 {
-    NSLog(@"autorelease: %@", [self class]);
+    void *addr[20];
+    int frame_count = backtrace(addr, sizeof(addr)/sizeof(*addr));
+    if (frame_count > 1) {
+        char **syms = backtrace_symbols(addr, frame_count);
+        for (int f = 0; f < frame_count; ++f) {
+            printf("%s caller: %s\n", __FUNCTION__, syms[f]);
+        }
+        printf("\n");
+        free(syms);
+    } else {
+        printf("%s: *** Failed to generate backtrace.", __FUNCTION__);
+    }
 
     ((void(*)(id))g_nsobject_autorelease)(self);
 
     return self;
+}
+@end
+
+@implementation NSAutoreleasePool(swizzle)
+- (void)fake_drain
+{
+    void *addr[20];
+    int frame_count = backtrace(addr, sizeof(addr)/sizeof(*addr));
+    if (frame_count > 1) {
+        char **syms = backtrace_symbols(addr, frame_count);
+        for (int f = 0; f < frame_count; ++f) {
+            printf("%s caller: %s\n", __FUNCTION__, syms[f]);
+        }
+        printf("\n");
+        free(syms);
+    } else {
+        printf("%s: *** Failed to generate backtrace.", __FUNCTION__);
+    }
+
+    ((void(*)(id))g_nsautoreleasepool_drain)(self);
 }
 @end
 
@@ -21,6 +54,12 @@ static bool hook_autorelease(void)
 
     Method m = class_getInstanceMethod(c, @selector(autorelease));
     g_nsobject_autorelease = method_setImplementation(m, (IMP)method_getImplementation(class_getInstanceMethod(c, @selector(fake_autorelease))));
+
+    Class c2 = objc_getClass("NSAutoreleasePool");
+    if (!c2) return false;
+
+    Method m2 = class_getInstanceMethod(c2, @selector(drain));
+    g_nsautoreleasepool_drain = method_setImplementation(m2, (IMP)method_getImplementation(class_getInstanceMethod(c2, @selector(fake_drain))));
 
     return true;
 }
