fcc9bfac946dddd3913639837828f40c1c40c5ef
koekeishiya <aasvi93@hotmail.com>
2022-03-16 17:11:10 +0100

    change to python3; python2.7 was removed in macOS 12.3

--- scripts/seticon.py
+++ scripts/seticon.py
@@ -3,8 +3,8 @@
 import Cocoa
 import sys
 
-image = Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1].decode('utf-8'));
-binary = sys.argv[2].decode('utf-8');
+image = Cocoa.NSImage.alloc().initWithContentsOfFile_(sys.argv[1]);
+binary = sys.argv[2];
 options = 0;
 
 result = Cocoa.NSWorkspace.sharedWorkspace().setIcon_forFile_options_(image, binary, options);
