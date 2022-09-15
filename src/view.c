b0b5ca480c8117ad2dac406ba7206d0fac0ae5d4
Antoine Charpentier <a.charpentier@cosling.com>
2022-09-15 14:40:35 +0200

    Additional check for insertion

--- src/view.c
+++ src/view.c
@@ -126,7 +126,7 @@ static inline enum window_node_split window_node_get_split(struct window_node *n
 
 static inline float window_node_get_ratio(struct window_node *node)
 {
-    return clampf_range(node->ratio, 0.1f, 0.9f);
+    return (node->ratio <= 0.0f) ? g_space_manager.split_ratio : clampf_range(node->ratio, 0.1f, 0.9f);
 }
 
 static inline float window_node_get_gap(struct view *view)
