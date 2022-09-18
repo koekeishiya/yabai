93fc3a3fbe5e04d04b150602d37e72d1eb9a9b0b
koekeishiya <aasvi93@hotmail.com>
2022-09-18 03:17:35 +0200

    #1421 fix hidden windows not showing in queries

--- src/space.c
+++ src/space.c
@@ -38,9 +38,11 @@ uint32_t *space_window_list_for_connection(uint64_t *space_list, int space_count
         uint64_t tags = SLSWindowIteratorGetTags(iterator);
         uint64_t attributes = SLSWindowIteratorGetAttributes(iterator);
         uint32_t parent_wid = SLSWindowIteratorGetParentID(iterator);
+        uint32_t wid = SLSWindowIteratorGetWindowID(iterator);
 
-        if ((parent_wid == 0) && ((attributes & 0x2) || (tags & 0x400000000000000)) && (((tags & 0x1)) || ((tags & 0x2) && (tags & 0x80000000)))) {
-            window_list[window_count++] = SLSWindowIteratorGetWindowID(iterator);
+        if ((window_manager_find_window(&g_window_manager, wid)) ||
+            ((parent_wid == 0) && ((attributes & 0x2) || (tags & 0x400000000000000)) && (((tags & 0x1)) || ((tags & 0x2) && (tags & 0x80000000))))) {
+            window_list[window_count++] = wid;
         }
     }
 
